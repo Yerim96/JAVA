4511152f22066ff6de0bbd932ad98d19afa2f210
Yerim96 <80906567+Yerim96@users.noreply.github.com>
2022-09-07 00:14:16 +0900

    6장 16번 문제
    6장 16번 문제 코드파일

---
+++ Printer.java
@@ -0,0 +1,29 @@
+package Homework;
+
+public class Printer {
+    //클래스에는 필드, 생성자, 메서드가 들어감.
+    //클래스에 필드가 반드시 있어야하는것은 아니다.
+
+    //메서드 선언 -> 반환값 메소드명(매개변수){}
+
+    //메서드 오버로딩
+    void println(int x){
+     int value= x;
+     System.out.println(value);
+    }
+
+    void println(boolean x){
+        boolean value = x;
+        System.out.println(value);
+    }
+
+    void println(double x){
+        double value = x;
+        System.out.println(value);
+    }
+
+    void println(String x){
+        String value = x;
+        System.out.println(value);
+    }
+}
---
+++ PrinterExample.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class PrinterExample {
+    public static void main(String[] args) {
+        //생성자 호출 -> 클래스명 변수이름 = new 클래스명
+        Printer printer = new Printer();
+        printer.println(10);
+        printer.println(true);
+        printer.println(5.7);
+        printer.println("홍길동");
+
+
+
+
+    }
+}
