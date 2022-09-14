8a75f7f5be02b3b4544e2712c074baaa242a1f11
Yerim96 <80906567+Yerim96@users.noreply.github.com>
2022-09-14 19:43:38 +0900

    8.11 인터페이서 매개변수의  다형성

---
+++ exam02/Driver.java
@@ -0,0 +1,7 @@
+package ch08.sec11.exam02;
+
+public class Driver {
+	void drive(Vehicle vehicle) {
+		vehicle.run(); //추상메서드의 run 메소드
+	}
+}
---
+++ exam02/Taxi.java
@@ -0,0 +1,10 @@
+package ch08.sec11.exam02;
+
+public class Taxi implements Vehicle {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	//추상메서드 재정의
+		System.out.println("택시가 달립니다.");
+	}
+}
