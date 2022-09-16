8afadbd614baf7acf8f30f4bf6376beddd35c5ae
Yerim96 <80906567+Yerim96@users.noreply.github.com>
2022-09-16 17:21:51 +0900

    11.3 예외 종류에 따른 처리

--- ExceptionHandlingExample.java
+++ ExceptionHandlingExample.java
@@ -1,17 +1,22 @@
-package ch11.sec02.exam01;
+package ch11.sec03.exam01;
 
 public class ExceptionHandlingExample {
-	public static void printLength(String data) {//data안들어오면 컴파일오류가남.
-		int result = data.length();
-		System.out.println("문자 수 :"+result);
-	}
 
 	public static void main(String[] args) {
-		System.out.println("[프로그램 시작]\n");
-		printLength(null);
-		System.out.println("[프로그램 종료]");
-
+	String[] array= {"100","1oo"};
+	
+	for(int i= 0; i<=2; i++) {
+		try{
+			System.out.println(array[i]); //예외발생1
+		int value = Integer.parseInt(array[i]); //예외발생2
+		
+		}catch(NumberFormatException e){
+			System.out.println("숫자로 변환할수 없는 항목이 있습니다. 확인해주십셔"); //예외발생2 출력
+		}catch(Exception e) {
+			System.out.println("항목수가 부족합니다. 다시 확인해주세요"); //예외발생1 출력
+		
+		}
 	}
 
+	}
 }
-
